f713a640d7f010538e7585032ff32004c95f7051
shibbe123 <122462709+shibbe123@users.noreply.github.com>
2023-02-23 16:53:08 +0100

    gjorde meny + knapp + text

--- demo/src/main/java/shibbe123/App.java
+++ demo/src/main/java/shibbe123/App.java
@@ -1,38 +1,67 @@
 package shibbe123;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+    public static void main(String[]args){
+        launch(args);
+        
+    }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        
+        primaryStage.setTitle("Harry Potter Game");
+        primaryStage.setWidth(840);
+        primaryStage.setHeight(680);
+       // primaryStage.setX(50);
+        //primaryStage.setY(50);
+        primaryStage.setFullScreen(true);
+
+        Text text = new Text();
+        text.setText("WELCOME TO THE GAME OF HARRY POTTER");
+        text.setX(170);
+        text.setY(100);
+        text.setFont(Font.font("Times New Roman", 50));
+        text.setFill(Color.CRIMSON);
+
+        Button button1 = new Button();
+        button1.setText("PLAY");
+        button1.setLayoutX(600);
+        button1.setLayoutY(250);
+        button1.setFont(Font.font("Times New Roman", 50));
+        //button1.setStyle("-fx-background-color: white;  -fx-border: 2px solid #4CAF50; -fx- padding: 16px 32px; -fx- cursor: pointer;");
+
+
+
+
+
+
+
+
+
+        root.getChildren().add(text);
+        root.getChildren().add(button1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+    
 
 }
